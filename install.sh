8b1d822d2433526853a9f7a29a227df807ed913f
somebody <noreply@example.org>
2017-04-02 21:08:47 +0800

    add X windows config

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@
 # Create Git global config symbolic link to home directory.
 ln -s ~/.config/git/.gitconfig ~/.gitconfig
 
+# X windows config
+ln -s ~/.config/X/.Xresources ~/.Xresources
+ln -s ~/.config/X/.xinitrc ~/.xinitrc
+
 # .zshrc
 ln -s ~/.config/zsh/.zshrc ~/.zshrc
 
